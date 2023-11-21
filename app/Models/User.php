a1692b3939618153528daf541b00369237e1aa43
Md Najmul Hasan <nh4023@gmail.com>
2023-11-21 14:21:29 +0600

    added lesson-11

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,13 +52,26 @@ class User extends Authenticatable
     {
         collect(str_getcsv($terms, ' ', '"'))->filter()->each(function ($term) use ($query){
             $term = $term.'%';
-            $query->where(function ($query) use ($term){
-                $query->where('first_name', 'like', $term)
-                    ->orWhere('last_name', 'like', $term)
-                    ->orWhereIn('company_id', Company::query()
-                        ->where('name', 'like', $term)
-                        ->pluck('id')
-                    );
+            // where in
+                // derived table
+                    // find user by first and last name
+                    // union
+                    // find user by company name
+            $query->whereIn('id', function ($query) use ($term) {
+               $query->select('id')
+                   ->from(function ($query) use ($term) {
+                        $query->select('id')
+                            ->from('users')
+                            ->where('first_name', 'like', $term)
+                            ->orWhere('last_name', 'like', $term)
+                            ->union(
+                                $query->newQuery()
+                                    ->select('users.id')
+                                    ->from('users')
+                                    ->join('companies', 'companies.id', '=', 'users.company_id')
+                                    ->where('companies.name', 'like', $term)
+                            );
+                   }, 'matches');
             });
         });
     }
