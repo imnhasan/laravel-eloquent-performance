bd39454c2a39687b19b4a703cde554474430ed67
Md Najmul Hasan <nh4023@gmail.com>
2023-11-27 12:25:19 +0600

    added lesson-15

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use function Laravel\Prompts\select;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+//        $users = User::query()
+//            ->orderBy('name')
+//            ->with('company')
+//            ->paginate();
+//        --------------------------------------------------------------------------------------------------------------
+
+//        $users = User::query()
+//            ->select('users.*')
+//            ->join('companies', 'companies.user_id', 'users.id')
+//            ->orderBy('companies.name')
+//            ->with('company')
+//            ->paginate();
+//        --------------------------------------------------------------------------------------------------------------
+
+        $users = User::query()
+            ->orderBy(Company::select('name')
+                ->whereColumn('user_id', 'users.id')
+                ->orderBy('name')
+                ->take(1)
+            )
+            ->with('company')
+            ->paginate();
+
+        return view('lesson-15.users', compact('users'));
+    }
+}
